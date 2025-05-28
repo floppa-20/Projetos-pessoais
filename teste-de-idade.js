b3d551d0bbcb2d5be40518b9cf03659b90b13c73
Luis Otávio Engler Bernardini <luisenglerbernadini@gmail.com>
2025-05-28 13:01:32 -0300

    Add files via upload

---
+++ teste-de-idade.js
@@ -0,0 +1,11 @@
+let i= prompt ("Digite sua idade: ")
+let age=Number(i)
+ if(age>100) {
+    alert ("Sério que você tem isso de idade? Seja honesto(a) pelo menos.")
+ }else if (age>=18) {
+    alert("Você é maior de idade!")
+}
+ else {
+    alert("Você é menor de idade!")
+}
+//isso só funciona no console do navegador, não sei porque...
